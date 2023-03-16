563a87a3a88ba481144c0a4fa61a21dcedd6567d
VillarroelJairo <jairitoiav11@gmail.com>
2023-03-15 22:26:05 -0300

    modified:   src/app/app.module.ts 	deleted:    src/app/componente1/componente1.component.css 	deleted:    src/app/componente1/componente1.component.html 	deleted:    src/app/componente1/componente1.component.spec.ts 	deleted:    src/app/componente1/componente1.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { Componente1Component } from './componente1/componente1.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    Componente1Component
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent, Componente1Component]
+  bootstrap: [AppComponent,]
 })
 export class AppModule { }
