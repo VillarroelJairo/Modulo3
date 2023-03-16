7f1bc4b43aa4e2cda06c60d63ff73d9f1a0b8ba0
VillarroelJairo <jairitoiav11@gmail.com>
2023-03-16 00:17:58 -0300

    modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	new file:   src/app/componentes/body/body.component.css 	new file:   src/app/componentes/body/body.component.html 	new file:   src/app/componentes/body/body.component.spec.ts 	new file:   src/app/componentes/body/body.component.ts 	new file:   src/app/componentes/footer/footer.component.css 	new file:   src/app/componentes/footer/footer.component.html 	new file:   src/app/componentes/footer/footer.component.spec.ts 	new file:   src/app/componentes/footer/footer.component.ts 	new file:   src/app/componentes/header/header.component.css 	new file:   src/app/componentes/header/header.component.html 	new file:   src/app/componentes/header/header.component.spec.ts 	new file:   src/app/componentes/header/header.component.ts 	new file:   src/app/componentes/header/login/login.component.css 	new file:   src/app/componentes/header/login/login.component.html 	new file:   src/app/componentes/header/login/login.component.spec.ts 	new file:   src/app/componentes/header/login/login.component.ts 	modified:   src/index.html 	modified:   src/styles.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { BodyComponent } from './componentes/body/body.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { LoginComponent } from './componentes/header/login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent,]
+  bootstrap: [AppComponent, HeaderComponent, BodyComponent, FooterComponent, LoginComponent]
 })
 export class AppModule { }
